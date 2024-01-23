10f726e6ce99a3ffeeb7e18562f91cefb0102ad5
Mikhail Ratner <an.ratner@gmail.com>
2024-01-23 21:50:12 +0100

    changed app to standalone structure with nx-inspired architecture

--- src/app/chat/data-access/chat.service.ts
+++ src/app/chat/data-access/chat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Message } from './models';
+import { Message } from '../../shared/models/models';
 
 @Injectable({
   providedIn: 'root',
--- src/app/chat/features/chat-window.component.ts
+++ src/app/chat/features/chat-window.component.ts
@@ -1,18 +1,50 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ChatService } from './chat.service';
-import { Message, ResponseOption, Sender, selectedResponseOption } from './models';
+import { Sender, Message, ResponseOption, selectedResponseOption } from 'src/app/shared/models/models';
+import { ChatService } from '../data-access/chat.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: 'app-chat-window',
+  standalone: true,
+  imports: [CommonModule],
+  template: `    
+  <div style="margin: 30px auto 30px auto; width: 800px; background: #F7F7F7; padding: 30px; border-radius: 10px">
+  <div class="panel" id="chat">
+    <div class="panel-body">
+      <div class="chats">
+        <div class="chat-window">
+          <div *ngFor="let message of messages" class="message">
+            <div [ngClass]="message.sender === senderEnum.BOT ? 'bot-message' : 'user-message'">
+              {{ message.content }}
+            </div>
+            <div *ngIf="message.sender === senderEnum.BOT && message.responseType === 'options'">
+              <button *ngFor="let option of message.responses" (click)="onUserResponse(message.id, option)" type="button" class="btn btn-secondary" style="margin: 6px">
+                {{ option.text }}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="panel-footer">
+      <form>
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Say something">
+          <span class="input-group-btn">
+            <button class="btn btn-primary" type="button">Send</button>
+          </span>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+    
+  `,
+  styles: ``,
 })
-export class AppComponent {
-  title = 'nAItingale';
-
-  senderEnum = Sender
-
+export class ChatWindowComponent {
+  senderEnum = Sender;
   messages: Message[] = [];
 
   constructor(private chatService: ChatService) {}
